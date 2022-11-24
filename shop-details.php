85d702da17a4c7e22ef032c5f3d0bec709c24359
Samuel Idowu Ogunbodede <samiwogdev@gmail.com>
2022-11-24 17:16:32 +0100

    shoping-details updated

--- shop-details.php
+++ shop-details.php
@@ -18,8 +18,8 @@ $shop_id = filter_input(INPUT_GET, "n");
                     <h1 class="page-headding"><?php echo $menus['name'] ?></h1>
                     <ul>
                         <li><a href="index" class="page-name">Home</a></li>
-                        <li><a href="#" class="page-name">Order Online</a></li>
-                        <li><?php echo $shops['name'] ?></li>
+                        <li><a href="#" class="page-name"><?php echo $shops['name'] ?></a></li>
+                        <li><?php echo $shops['description'] ?></li>
                     </ul>
                 </div>
             </div>
@@ -44,146 +44,21 @@ $shop_id = filter_input(INPUT_GET, "n");
             <div class="col-xl-7 col-lg-7 col-md-7">
                 <div class="shop-detail">
                     <div class="shop-name">
-                        <h3 class="title-shop">Paneer Makhani Pizza</h3>
+                        <h3 class="title-shop"><?php echo $shops['name'] ?></h3>
                         <div class="price-shop">
-                            <span class="filter-price filter-price-r">$ 30.50</span>
-                            <span class="filter-price">$ 20.50</span>
+                           <?php  $old_price = $shops['price'] * 30/100;?>
+                            <span class="filter-price filter-price-r">&#8358;<?php echo number_format($shops['price'] + $old_price )?></span>
+                            <span class="filter-price">&#8358;<?php echo number_format($shops['price'] )?></span>
                         </div>
-                        <p class="shop-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Proin lectus ipsum, gravida et mattis vulputate, tristique ut lectus. Sed et lorem nunc. ipsum primis in faucibus orci luctus et ultricesLorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
-                    </div>
-                    <div class="crust-choose">
-                        <label class="title-crust">Choose Your Crust</label>
-                        <ul class="Choose">
-                            <li class="tab-link"><a href="javascript:void(0)">Classic Hand Tossed</a></li>
-                            <li class="tab-link"><a href="javascript:void(0)">Wheat Thin Crust</a></li>
-                            <li class="tab-link"><a href="javascript:void(0)">Classic Hand Tossed</a></li>
-                        </ul>
-                    </div>
-                    <div class="crust-choose">
-                        <label class="title-crust">Size of Crust</label>
-                        <ul class="Size">
-                            <li class="tab-link"><a href="javascript:void(0)">Medium</a></li>
-                            <li class="tab-link"><a href="javascript:void(0)">Large</a></li>
-                            <li class="tab-link"><a href="javascript:void(0)">Regular</a></li>
-                        </ul>
+                        <p class="shop-desc"><?php echo $shops['description'] ?></p>
                     </div>
                     <div class="quantity-product">
-                        <label class="quantity">Qty:</label>
-                        <input type="number" value="1" min="0" max="10">
-                        <a href="cart" class="add-cart"><i class="fa fa-shopping-bag" aria-hidden="true"></i> Add to cart</a>
-                    </div>
-                    <div class="wiselist">
-                        <ul class="compare">
-                            <li><a href="shop-details"><i class="fa fa-heart" aria-hidden="true"></i> Wishlist </a></li>
-                            <li><a href="shop-details"><i class="fa fa-signal" aria-hidden="true"></i> Compare</a></li>
-                            <li><a href="shop-details"><i class="fa fa-envelope" aria-hidden="true"></i> Email to Friends</a></li>
-                        </ul>
-                        <ul class="share">
-                            <li class="share-title">Share This :</li>
-                            <li><a href="shop-details"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                            <li><a href="shop-details"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                            <li><a href="shop-details"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-                            <li><a href="shop-details"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-
-<section class="desc-tabbing pt-100">
-    <div class="container">
-        <div class="row">
-            <div class="col-xl-12 col-lg-12 col-md-12">
-                <div class="border-tab">
-                    <ul class="panel-tab">
-                        <li class="tab-link current" data-tab="tab-1"><a href="javascript:void(0)">Description</a></li>
-                        <li class="tab-link" data-tab="tab-3"><a href="javascript:void(0)">Reviews</a></li>
-                    </ul>
-                    <div class="product-desc-tab current" id="tab-1">
-                        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. </p>
-                        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-                    </div>
-                    <div class="product-desc-tab" id="tab-3">
-                        <div class="comment-part">
-                            <h3>03 COMMENTS</h3>
-                            <ul>
-                                <li>
-                                    <div class="comment-user">
-                                        <img src="images/comment-img1.jpg" alt="comment-img">
-                                    </div>
-                                    <div class="comment-detail">
-                                        <span class="commenter">
-                                            <span>John Doe</span> (05 Jan 2020)
-                                        </span>
-                                        <p>Lorem ipsum dolor sit amet adipiscing elit labore dolore that sed do eiusmod tempor labore dolore that magna aliqua. Ut enim ad minim veniam, exercitation.</p>
-                                        <a href="shop-details" class="reply-btn btn btn-color small">Reply</a>
-                                    </div>
-                                    <div class="clearfix"></div>
-                                </li>
-                                <li>
-                                    <ul>
-                                        <li>
-                                            <div class="comment-user">
-                                                <img src="images/comment-img2.jpg" alt="comment-img">
-                                            </div>
-                                            <div class="comment-detail">
-                                                <span class="commenter">
-                                                    <span>John Doe</span> (12 Jan 2020)
-                                                </span>
-                                                <p>Lorem ipsum dolor sit amet adipiscing elit labore dolore that sed do eiusmod tempor labore dolore that magna aliqua. Ut enim ad minim veniam, exercitation.</p>
-                                                <a href="shop-details" class="reply-btn btn btn-color small">Reply</a>
-                                            </div>
-                                            <div class="clearfix"></div>
-                                        </li>
-                                        <li>
-                                            <div class="comment-user">
-                                                <img src="images/comment-img3.jpg" alt="comment-img">
-                                            </div>
-                                            <div class="comment-detail">
-                                                <span class="commenter">
-                                                    <span>John Doe</span> (15 Jan 2020)
-                                                </span>
-                                                <p>Lorem ipsum dolor sit amet adipiscing elit labore dolore that sed do eiusmod tempor labore dolore that magna aliqua. Ut enim ad minim veniam, exercitation.</p>
-                                                <a href="shop-details" class="reply-btn btn btn-color small">Reply</a>
-                                            </div>
-                                            <div class="clearfix"></div>
-                                        </li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="leave-comment-part pt-100">
-                            <h3 class="reviews-head mb-30">Leave A Comment</h3>
-                            <form class="main-form">
-                                <div class="row">
-                                    <div class="col-md-4">
-                                        <div class="form-group">
-                                            <input type="text" class="form-control" placeholder="Name" required="">
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <div class="form-group">
-                                            <input type="text" class="form-control" placeholder="Subject" required="">
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <div class="form-group">
-                                            <input type="text" class="form-control" placeholder="Email" required="">
-                                        </div>
-                                    </div>
-                                    <div class="col-12">
-                                        <div class="form-group">
-                                            <textarea class="form-control" placeholder="Message" rows="8"></textarea>
-                                        </div>
-                                    </div>
-                                    <div class="col-12">
-                                        <button type="submit" class="btn post-comm">Post Comment</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
+                        <form action='' method="post">
+                            <label class="quantity">Qty:</label>
+                        <input type="number" name='quantity' value="1" min="0" max="10">
+                        <button type="submit" class="add-cart pt-3 pb-3"><i class="fa fa-shopping-bag" aria-hidden="true"></i>Add to cart</button>
+<!--                        <a href="cart" class="add-cart"><i class="fa fa-shopping-bag" aria-hidden="true"></i> Add to cart</a>-->
+                        </form>
                     </div>
                 </div>
             </div>
