546fc7e7ca0ca3a47d1059a2fa08370277154bc1
Samuel Idowu Ogunbodede <samiwogdev@gmail.com>
2022-11-29 12:27:01 +0100

    signup page added

--- signin.php
+++ signin.php
@@ -1,21 +1,21 @@
 <?php include_once './includes/header.php'; ?>
 
-	<section class="page-banner" style="background: #121619 url(images/blog-1.jpg) no-repeat center / cover;">
-		<div class="container">
-			<div class="row">
-				<div class="col-xl-12 col-lg-12 col-md-12">
-					<div class="page-title">
-						<h1 class="page-headding">Login</h1>
-						<ul>
-							<li><a href="index" class="page-name">Type your email to log in </a></li>
-<!--							<li><a href="cart" class="page-name">cart</a></li>
-							<li>CHECKOUT</li>-->
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
+<section class="page-banner" style="background: #121619 url(images/blog-1.jpg) no-repeat center / cover;">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12 col-lg-12 col-md-12">
+                <div class="page-title">
+                    <h1 class="page-headding">Login</h1>
+                    <ul>
+                        <li><a href="index" class="page-name">Type your email to log in </a></li>
+                        <!--							<li><a href="cart" class="page-name">cart</a></li>
+                                                                                <li>CHECKOUT</li>-->
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <section class="checkout-part ptb">
     <div class="container">
@@ -44,7 +44,7 @@
                                         <input id="company-name" type="text">
                                     </div>
                                 </div>
-
+                                <button class="btn full btn-color">Login</button>
                                 <div class="col-12">
                                     <div class="check-box mt-n2">
                                         <span>
