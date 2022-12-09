db9307ec40f55a7a495dce81a155d426a80d3a61
Samuel Idowu Ogunbodede <samiwogdev@gmail.com>
2022-12-09 10:15:04 +0100

    checkout  UI updated

--- checkout.php
+++ checkout.php
@@ -24,25 +24,27 @@
                         <div class="row">
                             <div class="col-12">
                                 <div class="estimate">
-                                        <h3 class="font-weight-bold mb-3 text-danger">Delivery Details</h3>
+                                        <h3 class="font-weight-bold mb-3">Delivery Method</h3>
                                     <form class="full main-form">
                                         <div class="row">
                                             <div class="col-md-12">
                                                 <div class="input-box">
                                                     <select id="del_option_id" class="full" onchange="deliveryMethod()">
-                                                        <option selected="" value="0">Select Delivery Method</option>
-                                                        <option value="1">Pick up at our shop</option>
+                                                        <option selected="" value="0">How do you want your order delivered ?</option>
                                                         <option value="2">Door delivery</option>
+                                                        <option value="1">Pickup Station (Cheaper Delivery Fees than Door Delivery)</option>
                                                     </select>
                                                 </div>
                                             </div>
-                                            <div class="col-md-12 mt-5" id="location_id" style="display: none">
+                                            <div class="col-md-12 mt-5 text-danger" id="location_id" style="display: none">
                                                 <div class="input-box">
-                                                    <label class="font-weight-bold">Location</label>
+                                                    <label class="font-weight-bold">Location *</label>
                                                     <select id="country_id">
-                                                        <option selected="" value="">Select your location</option>
-                                                        <option value="1">Pick up at our shop</option>
-                                                        <option value="2">Door delivery</option>
+                                                        <?php $citys = $delivery->get_all(); ?>
+                                                        <option selected="" value="">Select location</option>
+                                                         <?php foreach ($citys as $city){ ?>
+                                                        <option value="<?php echo $city['amount']?>"><?php echo $city['city'] ?></option>
+                                                         <?php } ?>
                                                     </select>
                                                 </div>
                                             </div>
@@ -55,16 +57,16 @@
                     <div class="row">
                         <div class="col-12" id="del_address_id" style="display: none">
 <!--                            <h3 class="font-weight-bold mb-3">Your Address</h3>-->
-                            <label class="font-weight-bold">Your Address</label>
+                            <label class="font-weight-bold text-danger">Your Address *</label>
                             <div class="notes p-4 mb-5">
-                                <p><span class="font-weight-bold pr-2" style="color: black">Location:</span>49, Joel Ogunnaike Street, Ikeja, Lagos, Nigeria.</p>
-                                <p><span class="font-weight-bold pr-2 text-black" style="color: black">Phone:</span>0907891085</p>
-                                <a href="shop-detail" class="btn btn-color mt-2" data-toggle="modal" data-target="#smallModal">Change Address</a>
+                                <p>49, Joel Ogunnaike Street, Ikeja, Lagos, Nigeria.</p>
+                                <p><span class="font-weight-bold pr-2 text-black">Phone:</span>0907891085</p>
+                                <a href="shop-detail" class="btn btn-color mt-2" data-toggle="modal" data-target="#smallModal">Change</a>
                             </div>
                         </div>
                        <div class="col-12 mt-5" id="pickup_address_id" style="display: none">
 <!--                            <h3 class="font-weight-bold mb-3">Pickup Address</h3>-->
-                            <label class="font-weight-bold">Pickup Address </label>
+                            <label class="font-weight-bold text-danger">Pickup Address </label>
                             <div class="notes p-4 mb-5">
                                 <p>49, Joel Ogunnaike Street, Ikeja, Lagos, Nigeria.</p>
                             </div>
@@ -120,7 +122,7 @@
                         </table>
                     </div>
                 </div>
-                <button class="btn full btn-color">Place order</button>
+                <button class="btn full btn-color" id="place_order_id" style="display: none">Place order</button>
             </div>
         </div>
     </div>
@@ -151,7 +153,7 @@
                             if (isset($_POST['phone'])) {
                                 echo $_POST['phone'];
                             }
-                            ?>" name="phone" class="form-control" placeholder="phone number">
+                            ?>" name="phone" class="form-control" placeholder="phone">
                         </div>
                     </div>
                       <div class="col-lg-12 col-12 mt-3">
@@ -163,7 +165,7 @@
                             if (isset($_POST['address'])) {
                                 echo $_POST['address'];
                             }
-                            ?>" name="address" class="form-control" placeholder="Delivery address">
+                            ?>" name="address" class="form-control" placeholder="address">
                         </div>
                     </div>
                 </div>
@@ -180,13 +182,16 @@
          var location = document.getElementById('location_id');
         var address = document.getElementById('del_address_id');
         var pickup_address = document.getElementById('pickup_address_id');
+        var place_order_but = document.getElementById('place_order_id');
         if (deliveryOption == 1) {
             pickup_address.style.display = "block";
+            place_order_but.style.display = "block";
             location.style.display = "none"; 
             address.style.display = "none"; 
         }
         else if(deliveryOption == 2){
           location.style.display = "block"; 
+          place_order_but.style.display = "block";
           address.style.display = "block";
           pickup_address.style.display = "none";
         }
